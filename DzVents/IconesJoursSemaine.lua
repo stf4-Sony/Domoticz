4499f4f86fbe45cf0361f3bff6b41552fbc4d896
McFlyPartages <44530252+McFlyPartages@users.noreply.github.com>
2020-01-03 00:06:32 +0100

    Create IconesJoursSemaine.lua
    Copie du fichier de papo-o https://github.com/papo-o

---
+++ DzVents/IconesJoursSemaine.lua
@@ -0,0 +1,47 @@
+--[[ daysSelectorIcons.lua
+source : https://www.domoticz.com/forum/viewtopic.php?f=59&t=27501&p=210819&hilit=seticon#p210819
+https://pon.fr/dzvents-afficher-le-jour-de-la-semaine-sous-forme-d-icone/
+https://easydomoticz.com/forum/
+icones : https://github.com/papo-o/domoticz_scripts/tree/master/Icons/Days
+ update/mise à jour = 27/12/2019
+--]]
+local scriptName        = 'daysSelectorIcons'
+local scriptVersion     = ' 1.02'
+return  {   
+        active = true,
+        on =    {  
+                       devices         = {1927}, -- change to your device(s) separated by a comma like {2479,2480} 
+                    },
+
+   logging =   {  level    =   domoticz.LOG_DEBUG,                                             -- Seulement un niveau peut être actif; commenter les autres
+                -- -- -- level    =   domoticz.LOG_INFO,                                            -- Only one level can be active; comment others
+                -- -- -- level    =   domoticz.LOG_ERROR,
+                -- -- -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
+                 marker = scriptName..' v'..scriptVersion
+    },
+
+    execute = function(dz, item)
+    
+        local function logWrite(str,level)
+            dz.log(tostring(str),level or dz.LOG_DEBUG)
+        end
+        local icons =     { 
+                            [10] = 124,-- level correspondant à dimanche
+                            [20] = 118,-- level correspondant à lundi
+                            [30] = 119,-- level correspondant à mardi
+                            [40] = 120,-- level correspondant à mercredi
+                            [50] = 121,-- level correspondant à jeudi
+                            [60] = 122,-- level correspondant à vendredi
+                            [70] = 123,-- level correspondant à samedi
+
+                        }
+           
+        logWrite('lastLevel '.. tostring(item.lastLevel))
+        logWrite('level '.. tostring(item.level))
+        if item.level ~= item.lastLevel then 
+            item.setIcon(icons[item.level])
+        else
+            logWrite('No Icon change necessary for ' .. item.id)
+        end
+    end
+}
