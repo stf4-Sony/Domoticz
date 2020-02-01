4b12ae4b439b5d43d0501e9dbea237474bbce5a4
McFlyPartages <44530252+McFlyPartages@users.noreply.github.com>
2020-02-01 23:28:16 +0100

    Update and rename ExplicationDesCapteursVirtuel to ExplicationDesCapteursVirtuel.lua

---
+++ enTest/InfosTravail/ExplicationDesCapteursVirtuel.lua
@@ -0,0 +1,10 @@
+
+
+
+--[[ capteur index UV 
+ /json.htm?type=command&param=udevice&idx=IDX&nvalue=0&svalue=COUNTER;0
+--]]
+    IDX = idx de votre capteur. A obtenir depuis l’écran des dispositifs.
+    COUNTER = La valeur de l’UV avec un point décimal (obligatoire : exemple: 2.0)
+
+Attention il y a un « ;0 » à la fin, c’est comme ça , ne l’oubliez pas.
